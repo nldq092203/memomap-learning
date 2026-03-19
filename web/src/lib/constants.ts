949727d5a41156b84436f1ce9febf2232a7d6206
nldq092203 <lnguye220903@gmail.com>
2026-03-19 21:20:08 +0100

    feat: community page

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -3,7 +3,7 @@
  */
 
 export const APP_SURFACE_HEX = "#FFFBF1"
-export const APP_SURFACE_RGB = "255 251 241"
+export const APP_SURFACE_RGB = "255 255 255"
 
 // Base URL for raw content fetched directly from the memomap-audio-fr GitHub repository
 export const GITHUB_CONTENT_BASE_URL = "https://raw.githubusercontent.com/nldq092203/memomap-audio-fr/main"
